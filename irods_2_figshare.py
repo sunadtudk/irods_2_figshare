70246b7170d50bf9548dd2f16ff10ad17b7bdb28
Sune Andersen <sunadtudk@gmail.com>
2016-05-19 16:47:30 +0200

    Update irods_2_figshare.py
    Now with help to get it to run!

--- irods_2_figshare.py
+++ irods_2_figshare.py
@@ -1,6 +1,14 @@
+# please note!  
+# please do before playing with figshare: 
+# yum update ; yum -y install python-pip
+# pip install git+git://github.com/iPlantCollaborativeOpenSource/pycommands.git
+# pip install --upgrade pip
+# pip install --upgrade git+git://github.com/irods/python-irodsclient.git
+# pip install pigshare
+#
 # irods_2_figshare
-#By Sune Andersen
-# version 0.1a
+# By Sune Andersen
+# version 0.1b   
 import fileinput
 import requests
 import os
@@ -11,7 +19,7 @@ from irods.models import Collection, User, DataObject
 sess = iRODSSession(host='localhost', port=1247, user='irodsuser', password='XxxxxxxA', zone='MyZone')
 coll = sess.collections.get("/MyZone/home/irodsuser")
 
-fileout = open('workfile', 'w')
+fileout = open('figshare_test.txt', 'w')
 
 objf = sess.data_objects.get("/MyZone/home/irodsuser/figshare_test.txt")
 with objf.open('r+') as f:
