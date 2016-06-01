6c427a5eb67e92a32127efac03d36f3fe504cdb7
Sune Andersen <sunadtudk@gmail.com>
2016-06-01 10:11:22 +0200

    Update irods_2_figshare.py

--- irods_2_figshare.py
+++ irods_2_figshare.py
@@ -8,26 +8,26 @@
 #  Do a "pigshare -h"  to see the API ;-)
 # irods_2_figshare
 # By Sune Andersen
-# version 0.1b   
+# version 0.2   
+# -*- coding: utf-8 -*-
 import fileinput
 import requests
 import os
-from irods import *
+from pigshare.api import figshare_api
+api = figshare_api(url="https://api.figshare.com/v2")
 from irods.session import iRODSSession
 from irods.models import Collection, User, DataObject
 
-sess = iRODSSession(host='localhost', port=1247, user='irodsuser', password='XxxxxxxA', zone='MyZone')
-coll = sess.collections.get("/MyZone/home/irodsuser")
+sess = iRODSSession(host='localhost', port=1247, user='irodsuser', password='xxxxxxxxxxxxx', zone='DTU')
+coll = sess.collections.get("/DTU/home/irodsuser")
 
-fileout = open('figshare_test.txt', 'w')
+fileout = open('big_data.jpg', 'w')
 
-objf = sess.data_objects.get("/MyZone/home/irodsuser/figshare_test.txt")
+objf = sess.data_objects.get("/DTU/home/irodsuser/big_data.jpg")
 with objf.open('r+') as f:
    for line in f:
      fileout.write(line)
-#   Test : 
-#BASH #   pigshare create_article --article '{"title": "irods test", "custom_fields": {"key1": "from_irods"}}'
-#{
-# "location": "https://api.figshare.com/v2/account/articles/3393214"    <----  This number is the article ID for upload
-#}
-#BASH #    pigshare upload_new_file --id 3393214 figshare_test.txt
+
+#########  Test figshare API#############
+#result = api.call_list_articles()
+#print result
